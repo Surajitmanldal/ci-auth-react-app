43d854b4aec5f4c8d1ad34934f65a43416f76b8d
surajitmandal <surajitmandal5631@gmail.com>
2026-03-28 14:52:53 +0530

    navigate problem solved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,16 +5,15 @@ import Dashboard from "./pages/Dashboard";
 import Users from "./pages/Users";
 import Teachers from "./pages/Teachers";
 function App() {
-  const token = localStorage.getItem("token");
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={token ? <Dashboard /> : <Register />} />
-        <Route path="/login" element={!token ? <Login /> : <Dashboard />} />
-        <Route path="/dashboard" element={token ? <Dashboard /> : <Login />} />
-        <Route path="/users" element={token ? <Users /> : <Login />} />
-        <Route path="/teachers" element={token ? <Teachers /> : <Login />} />
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/teachers" element={<Teachers />} />
       </Routes>
 
     </BrowserRouter>
